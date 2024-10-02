56c92e79561b4e8a1e4d03dfc3c90ee5bdb21198
Abidi Yassine <abidiyassine@outlook.com>
2024-10-02 07:12:27 +0100

    first changes

---
+++ src/main/java/com/example/React_back/Models/Admin.java
@@ -0,0 +1,13 @@
+package com.example.React_back.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Admin extends User{
+}
